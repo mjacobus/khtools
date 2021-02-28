91f62445a8c7f4a0a15a4f1221098444f32c63dc
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-02-28 14:58:42 -0300

    Remove migrations

--- db/migrate/20210126235150_add_last_contacted_to_phones.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddLastContactedToPhones < ActiveRecord::Migration[6.1]
-  def change
-    add_column :phones, :last_contacted_at, :datetime, default: nil
-  end
-end
--- db/migrate/20210206124018_add_last_called_at.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddLastCalledAt < ActiveRecord::Migration[6.1]
-  def change
-    add_column :phones, :last_called_at, :datetime, default: nil
-  end
-end
