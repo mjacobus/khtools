1c14fbea03ee3028216d1b1a876918a94fb20ac3
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-03-01 00:30:39 -0300

    Add form for simple counter attendee

---
+++ spec/forms/meeting_attendance/simple_counter_attendee_form_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MeetingAttendance::SimpleCounterAttendeeForm do
+  let(:form) { described_class.new(model) }
+  let(:model) { Db::MeetingAttendance::SimpleCounterAttendee.new }
+
+  describe '#model_name' do
+    it 'return the correct model name' do
+      expect(form.model_name.param_key).to eq('attendee')
+      expect(form.model_name.singular_route_key).to eq('meeting_attendance_simple_counter_attendee')
+    end
+  end
+end
