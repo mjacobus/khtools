426cf0df3a30e013ec4d3ae35798161335557e38
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-12 10:25:26 -0300

    WIP

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,26 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
 
-import "bootstrap";
-import "../src/aplicattion.css"; 
+// import "bootstrap";
+// import "../src/aplicattion.scss"; 
+alert("aaa")
 
+// require("bootstrap")
+import "../src/application";
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
+
+
+alert("aaa")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
