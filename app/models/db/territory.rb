b47fa9f7931deda9eadd3d0b94a2590a9f60e579
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-15 16:00:57 -0300

    Change default order for territories

--- app/models/db/territory.rb
+++ app/models/db/territory.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Db::Territory < ApplicationRecord
+  default_scope { order(:name) }
+
   validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :type }
   belongs_to :assignee, class_name: 'Publisher', optional: true
 end
