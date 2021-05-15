dddece5a68a2a3163261839dbd7e25d872609a88
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-15 17:50:16 -0300

    Add UI for phone list territory

---
+++ app/admin/db_phone_list_territories.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Db::PhoneListTerritory do
+  menu parent: 'Territórios'
+  permit_params :name,
+                :assigned_at,
+                :returned_at,
+                :type,
+                :assignee_id,
+                :initial_phone_number,
+                :final_phone_number
+
+  index do
+    selectable_column
+    column :name
+    column :initial_phone_number
+    column :final_phone_number
+    column :assignee
+    column :assigned_at
+    column :returned_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :assigned_at
+      row :returned_at
+      row :assignee
+      row :initial_phone_number
+      row :final_phone_number
+      row :numbers do |record|
+        ul do
+          record.phone_numbers.each do |number|
+            li number
+          end
+        end
+      end
+    end
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :name
+      f.input :initial_phone_number
+      f.input :final_phone_number
+      f.input :assigned_at
+      f.input :returned_at
+      f.input :assignee
+    end
+    f.actions
+  end
+end
--- app/models/db/phone_list_territory.rb
+++ app/models/db/phone_list_territory.rb
@@ -3,4 +3,12 @@
 class Db::PhoneListTerritory < Db::Territory
   validates :initial_phone_number, presence: true
   validates :final_phone_number, presence: true
+
+  def phone_numbers
+    if initial_phone_number.present? && final_phone_number.present?
+      return (initial_phone_number..final_phone_number).to_a
+    end
+
+    []
+  end
 end
