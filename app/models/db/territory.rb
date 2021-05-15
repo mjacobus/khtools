9798328cd7da978c33fb74a6750b55fd03fec574
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-15 20:32:38 -0300

    CS Fix

--- app/models/db/territory.rb
+++ app/models/db/territory.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Db::Territory < ApplicationRecord
+  belongs_to :assignee, class_name: 'Publisher', optional: true
+
   default_scope { order(:name) }
 
   validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :type }
-  belongs_to :assignee, class_name: 'Publisher', optional: true
 end
