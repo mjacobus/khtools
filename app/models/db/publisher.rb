300e34bb87973b586d73988c10a9cb968e0c529d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-15 16:00:22 -0300

    Change default scope for publisher

--- app/models/db/publisher.rb
+++ app/models/db/publisher.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Db::Publisher < ApplicationRecord
+  default_scope { order(:name) }
   belongs_to :group, class_name: 'FieldServiceGroup'
 
   validates :name, presence: true
