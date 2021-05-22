63ca05939820a82775d426ca7e237615367a3bf0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-22 16:40:28 -0300

    Add territory validations

--- app/models/db/apartment_building_territory.rb
+++ app/models/db/apartment_building_territory.rb
@@ -1,4 +1,31 @@
 # frozen_string_literal: true
 
 class Db::ApartmentBuildingTerritory < Db::Territory
+  PRECHING_METHODS = %w[
+    itermcom
+    letters
+  ].freeze
+
+  LETTER_BOX_TYPES = %w[
+    individual
+    intern
+    unique
+    inexistent
+  ].freeze
+
+  INTERCOM_TYPES_TYPES = %w[
+    individual
+    intern
+    unique
+    inexistent
+  ].freeze
+
+  belongs_to :area, class_name: 'Db::TerritoryArea'
+  validates :address, presence: true
+  validates :number_of_apartments, presence: true
+  validates :primary_preaching_method, inclusion: { in: PRECHING_METHODS }
+  validates :secondary_preaching_method, inclusion: { in: PRECHING_METHODS }
+  validates :tertiary_preaching_method, inclusion: { in: PRECHING_METHODS }
+  validates :letter_box_type, inclusion: { in: LETTER_BOX_TYPES }
+  validates :intercom_type, inclusion: { in: INTERCOM_TYPES_TYPES }
 end
--- spec/models/db/apartment_building_territory_spec.rb
+++ spec/models/db/apartment_building_territory_spec.rb
@@ -6,7 +6,17 @@ RSpec.describe Db::ApartmentBuildingTerritory, type: :model do
   let(:factory) { factories.apartment_building_territories }
   let(:territory) { factory.build }
 
-  it 'persists' do
-    expect { territory.save! }.to change(described_class, :count).by(1)
-  end
+  it { is_expected.to validate_presence_of(:number_of_apartments) }
+  it { is_expected.to validate_presence_of(:address) }
+  it { is_expected.to belong_to(:area).class_name('Db::TerritoryArea') }
+  # it { is_expected.to validate_inclusion_of(:primary_preaching_method)
+  #   .in_array(%w[letters intercom]) }
+  # it { is_expected.to validate_inclusion_of(:secondary_preaching_method)
+  #   .in_array(%w[letters intercom]) }
+  # it { is_expected.to validate_inclusion_of(:tertiary_preaching_method)
+  #   .in_array(%w[letters intercom]) }
+  # it { is_expected.to validate_inclusion_of(:letter_box_type)
+  #   .in_array(%w[letters intercom]) }
+  # it { is_expected.to validate_inclusion_of(:intercom_type)
+  #   .in_array(%w[letters intercom]) }
 end
