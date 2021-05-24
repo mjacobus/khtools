c22cf630a1655897f8c8a6a3a7bede776e541a40
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-23 21:20:06 -0300

    Whitelist fields for csv import service

--- app/services/apartment_building_territory_csv_import_service.rb
+++ app/services/apartment_building_territory_csv_import_service.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 class ApartmentBuildingTerritoryCsvImportService
+  ACCEPTED_HEADERS = %i[
+    name
+    publisher
+    building_name
+    address
+    area
+    number_of_apartments
+    primary_preaching_method
+    secondary_preaching_method
+    tertiary_preaching_method
+    has_a_roof
+    intercom_type
+    letter_box_type
+    notes
+    territory
+    apartments
+  ].freeze
+
   def import_file(file)
     Db::ApartmentBuildingTerritory.transaction do
       CSV.parse(file, headers: true) do |row|
@@ -13,8 +31,8 @@ class ApartmentBuildingTerritoryCsvImportService
 
   # rubocop:disable Metrics/MethodLength
   def import_row(row)
+    row = filter_row(row)
     parent = row.delete(:territory)
-
     area = row.delete(:area)
     assignee = row.delete(:assignee)
 
@@ -34,5 +52,12 @@ class ApartmentBuildingTerritoryCsvImportService
 
     territory.save!
   end
+
+  def filter_row(row)
+    row.select do |key, _value|
+      ACCEPTED_HEADERS.include?(key)
+    end
+  end
+
   # rubocop:enable Metrics/MethodLength
 end
