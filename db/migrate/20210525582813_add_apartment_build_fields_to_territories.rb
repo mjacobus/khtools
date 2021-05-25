028fe7289542c57ed0828e1dea1321035b278e9f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-24 21:11:59 -0300

    Fix apartment building migration

--- db/migrate/20210525582813_add_apartment_build_fields_to_territories.rb
+++ db/migrate/20210525582813_add_apartment_build_fields_to_territories.rb
@@ -6,9 +6,6 @@ class AddApartmentBuildFieldsToTerritories < ActiveRecord::Migration[6.1]
     add_column :territories, :building_name, :string
     add_reference :territories, :territory_area, foreign_key: true
     add_column :territories, :number_of_apartments, :integer
-    add_column :territories, :primary_preaching_method, :string, index: true
-    add_column :territories, :secondary_preaching_method, :string, index: true
-    add_column :territories, :tertiary_preaching_method, :string, index: true
     add_column :territories, :has_a_roof, :boolean, index: true, null: true
     add_column :territories, :intercom_type, :string, index: true
     add_column :territories, :letter_box_type, :string, index: true
@@ -17,5 +14,15 @@ class AddApartmentBuildFieldsToTerritories < ActiveRecord::Migration[6.1]
 
     add_reference :territories, :territory, foreign_key: { to_table: :territories }
     add_reference :territories, :area, foreign_key: { to_table: :territory_areas }
+
+    add_reference :territories, :intercom_type,
+                  foreign_key: { to_table: :intercom_types }
+
+    add_reference :territories, :primary_preaching_method,
+                  foreign_key: { to_table: :preaching_methods }
+    add_reference :territories, :secondary_preaching_method,
+                  foreign_key: { to_table: :preaching_methods }
+    add_reference :territories, :tertiary_preaching_method,
+                  foreign_key: { to_table: :preaching_methods }
   end
 end
