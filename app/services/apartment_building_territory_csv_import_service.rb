0fd9ba60417449684dde0e520683912e6663267a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-24 22:04:17 -0300

    Improve CSV importer

--- app/services/apartment_building_territory_csv_import_service.rb
+++ app/services/apartment_building_territory_csv_import_service.rb
@@ -29,6 +29,9 @@ class ApartmentBuildingTerritoryCsvImportService
 
   private
 
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
   # rubocop:disable Metrics/MethodLength
   def import_row(row)
     row = filter_row(row)
@@ -38,6 +41,8 @@ class ApartmentBuildingTerritoryCsvImportService
     primary_preaching_method = row.delete(:primary_preaching_method)
     secondary_preaching_method = row.delete(:secondary_preaching_method)
     tertiary_preaching_method = row.delete(:tertiary_preaching_method)
+    letter_box_type = row.delete(:letter_box_type)
+    intercom_type = row.delete(:intercom_type)
 
     territory = Db::ApartmentBuildingTerritory.new(row)
 
@@ -54,25 +59,43 @@ class ApartmentBuildingTerritoryCsvImportService
     end
 
     if primary_preaching_method
-      territory.primary_preaching_method = Db::PreachingMethod.find_or_create_by(name: primary_preaching_method)
+      territory.primary_preaching_method = Db::PreachingMethod.find_or_create_by(
+        name: primary_preaching_method
+      )
     end
 
     if secondary_preaching_method
-      territory.secondary_preaching_method = Db::PreachingMethod.find_or_create_by(name: secondary_preaching_method)
+      territory.secondary_preaching_method = Db::PreachingMethod.find_or_create_by(
+        name: secondary_preaching_method
+      )
     end
 
     if tertiary_preaching_method
-      territory.tertiary_preaching_method = Db::PreachingMethod.find_or_create_by(name: tertiary_preaching_method)
+      territory.tertiary_preaching_method = Db::PreachingMethod.find_or_create_by(
+        name: tertiary_preaching_method
+      )
+    end
+
+    if letter_box_type
+      territory.letter_box_type = Db::LetterBoxType.find_or_create_by(
+        name: letter_box_type
+      )
+    end
+
+    if intercom_type
+      territory.intercom_type = Db::IntercomType.find_or_create_by(name: intercom_type)
     end
 
     territory.save!
   end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/PerceivedComplexity
+  # rubocop:enable Metrics/MethodLength
 
   def filter_row(row)
     row.select do |key, _value|
       ACCEPTED_HEADERS.include?(key)
     end
   end
-
-  # rubocop:enable Metrics/MethodLength
 end
