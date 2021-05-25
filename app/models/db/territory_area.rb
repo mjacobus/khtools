298ffd9cff5cc20f0d6c421a70b01a1e4166a17a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-25 09:43:20 -0300

    Restrict territory area deletion

--- app/models/db/territory_area.rb
+++ app/models/db/territory_area.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Db::TerritoryArea < ApplicationRecord
+  has_many :territories,
+           class_name: 'Territory',
+           inverse_of: :area,
+           foreign_key: :area_id,
+           dependent: :restrict_with_exception
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
