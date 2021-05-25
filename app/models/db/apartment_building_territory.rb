b115403862f696b7c7c492845036c07658cd643d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-25 09:29:18 -0300

    Restrict preaching method deletion

--- app/models/db/apartment_building_territory.rb
+++ app/models/db/apartment_building_territory.rb
@@ -1,22 +1,5 @@
 # frozen_string_literal: true
 
 class Db::ApartmentBuildingTerritory < Db::Territory
-  belongs_to :area, class_name: 'Db::TerritoryArea', optional: true
-  belongs_to :primary_preaching_method,
-             class_name: 'Db::PreachingMethod',
-             optional: true
-  belongs_to :secondary_preaching_method,
-             class_name: 'Db::PreachingMethod',
-             optional: true
-  belongs_to :tertiary_preaching_method,
-             class_name: 'Db::PreachingMethod',
-             optional: true
-  belongs_to :intercom_type,
-             class_name: 'Db::IntercomType',
-             optional: true
-  belongs_to :letter_box_type,
-             class_name: 'Db::LetterBoxType',
-             optional: true
-
   validates :address, presence: true
 end
