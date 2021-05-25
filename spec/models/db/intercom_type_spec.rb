bdb29b624cbef3a309168a50982bb901b2408c44
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-25 09:36:30 -0300

    Restrict intercom type deletion

--- spec/models/db/intercom_type_spec.rb
+++ spec/models/db/intercom_type_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Db::IntercomType, type: :model do
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+
+  it 'has many territories' do
+    expect(type).to have_many(:territories)
+      .class_name('Db::Territory')
+      .inverse_of(:intercom_type)
+      .dependent(:restrict_with_exception)
+  end
 end
