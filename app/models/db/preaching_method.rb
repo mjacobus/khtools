0ccc0774aabc03acd7af376f7e299814a2bf301c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-25 09:29:18 -0300

    Restrict preaching method deletion

--- app/models/db/preaching_method.rb
+++ app/models/db/preaching_method.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
 class Db::PreachingMethod < ApplicationRecord
+  has_many :primary_territories,
+           class_name: 'Territory',
+           foreign_key: :primary_preaching_method_id,
+           inverse_of: :primary_preaching_method,
+           dependent: :restrict_with_exception
+
+  has_many :secondary_territories,
+           class_name: 'Territory',
+           foreign_key: :secondary_preaching_method_id,
+           inverse_of: :secondary_preaching_method,
+           dependent: :restrict_with_exception
+
+  has_many :tertiary_territories,
+           class_name: 'Territory',
+           foreign_key: :tertiary_preaching_method_id,
+           inverse_of: :tertiary_preaching_method,
+           dependent: :restrict_with_exception
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- app/models/db/territory.rb
+++ app/models/db/territory.rb
@@ -3,6 +3,22 @@
 class Db::Territory < ApplicationRecord
   belongs_to :assignee, class_name: 'Publisher', optional: true
   belongs_to :territory, class_name: 'Territory', optional: true
+  belongs_to :area, class_name: 'Db::TerritoryArea', optional: true
+  belongs_to :primary_preaching_method,
+             class_name: 'Db::PreachingMethod',
+             optional: true
+  belongs_to :secondary_preaching_method,
+             class_name: 'Db::PreachingMethod',
+             optional: true
+  belongs_to :tertiary_preaching_method,
+             class_name: 'Db::PreachingMethod',
+             optional: true
+  belongs_to :intercom_type,
+             class_name: 'Db::IntercomType',
+             optional: true
+  belongs_to :letter_box_type,
+             class_name: 'Db::LetterBoxType',
+             optional: true
 
   default_scope { order(:name) }
   identifiable_by :name
