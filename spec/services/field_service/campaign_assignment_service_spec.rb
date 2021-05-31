99a3eaaa3649e28cd3330d9bc61b17069fabadf5
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-31 20:29:27 -0300

    Add method #exist? to campaign assignment service

--- spec/services/field_service/campaign_assignment_service_spec.rb
+++ spec/services/field_service/campaign_assignment_service_spec.rb
@@ -5,16 +5,16 @@ require 'rails_helper'
 RSpec.describe FieldService::CampaignAssignmentService do
   subject(:service) { described_class.new }
 
-  describe '#create' do
-    let(:campaign) { factories.preaching_campaigns.create }
-    let(:types) do
-      {
-        apartment_building_territory: factories.apartment_building_territories.create,
-        phone_list_territory: factories.phone_list_territories.create,
-        regular_territory: factories.territories.create
-      }
-    end
+  let(:types) do
+    {
+      apartment_building_territory: factories.apartment_building_territories.create,
+      phone_list_territory: factories.phone_list_territories.create,
+      regular_territory: factories.territories.create
+    }
+  end
+  let(:campaign) { factories.preaching_campaigns.create }
 
+  describe '#create' do
     it 'creates assignments for each one of the territory types' do
       types.each do |type, territory|
         expect { service.create(territory_type: type, campaign_id: campaign.id) }
@@ -24,6 +24,20 @@ RSpec.describe FieldService::CampaignAssignmentService do
 
         expect(last.campaign_id).to eq(campaign.id)
         expect(last.territory).to eq(territory)
+      end
+    end
+  end
+
+  describe '#exist?' do
+    let(:other_campaign) { factories.preaching_campaigns.create }
+
+    it 'returns true' do
+      types.each do |type, _territory|
+        service.create(territory_type: type, campaign_id: other_campaign.id)
+
+        expect { service.create(territory_type: type, campaign_id: campaign.id) }
+          .to change { service.exist?(territory_type: type, campaign_id: campaign.id) }
+          .from(false).to(true)
       end
     end
   end
