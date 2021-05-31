b1a49220467e595deff7259d5df9b25f8993faf0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-31 16:54:26 -0300

    Fix TestFactories

--- app/controllers/field_service/campaign_assignments_controller.rb
+++ app/controllers/field_service/campaign_assignments_controller.rb
@@ -3,24 +3,13 @@
 class FieldService::CampaignAssignmentsController < ApplicationController
   def index
     @campaign = Db::PreachingCampaign.find(params[:campaign_id])
-    @assignments = @campaign.assignments.includes(:territory, :assignee)
   end
 
-  # def show
-  # end
-  #
-  # def new
-  # end
-  #
-  # def create
-  # end
-  #
-  # def edit
-  # end
-  #
-  # def update
-  # end
-  #
-  # def destroy
-  # end
+  def edit
+    # TODO
+  end
+
+  def update
+    # TODO
+  end
 end
