c829980999a609d0c58155082ce12d52a70b3e2d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-06-09 07:36:36 -0300

    Refactor links in the navbar

--- app/components/sidebar/sidebar_component.rb
+++ app/components/sidebar/sidebar_component.rb
@@ -6,4 +6,47 @@ class Sidebar::SidebarComponent < ApplicationComponent
   def render?
     current_user
   end
+
+  def menu_entries
+    [
+      home_link,
+      meeting_attendance,
+      field_service_campaigns,
+      admin_dashboard,
+      users,
+      logout
+    ].compact
+  end
+
+  private
+
+  def home_link
+    entry('Home', root_path)
+  end
+
+  def meeting_attendance
+    entry(t('app.links.meeting_attendance'), meeting_attendance_meetings_url)
+  end
+
+  def admin_dashboard
+    if current_user.master?
+      entry('Admin', admin_dashboard_url)
+    end
+  end
+
+  def users
+    entry(t('app.links.users'), users_path)
+  end
+
+  def field_service_campaigns
+    entry(t('app.links.preaching_campaigns'), field_service_campaigns_url)
+  end
+
+  def logout
+    entry(t('app.links.logout'), '/logout')
+  end
+
+  def entry(*args)
+    Sidebar::MenuEntry.new(*args)
+  end
 end
