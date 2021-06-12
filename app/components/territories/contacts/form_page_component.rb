c264fbe8b6cdb704d29ef5e6f735a6e40c6baa91
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-06-12 18:05:52 -0300

    Add breadcrumb component
    It's looks still need to be improved

--- app/components/territories/contacts/form_page_component.rb
+++ app/components/territories/contacts/form_page_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-class Territories::Contacts::FormPageComponent < ApplicationComponent
+class Territories::Contacts::FormPageComponent < PageComponent
   attr_reader :contact
 
   def initialize(territory:, contact:)
     @contact = contact
     @territory = territory
+    setup_breadcrumb
   end
 
   def page_title
@@ -18,5 +19,27 @@ class Territories::Contacts::FormPageComponent < ApplicationComponent
     end
 
     territories_commercial_territory_contacts_path(@territory)
+  end
+
+  private
+
+  def setup_breadcrumb
+    breadcrumb.add_item(
+      t('app.links.commercial_territories'),
+      urls.territories_commercial_territories_path
+    )
+
+    breadcrumb.add_item(
+      @territory.name
+      # urls.territories_commercial_territory_path(@territory) # no action yet
+    )
+
+    breadcrumb.add_item(
+      t('app.links.contacts'),
+      urls.territories_commercial_territory_contacts_path(@territory)
+    )
+
+    action = contact.id ? 'edit' : 'new'
+    breadcrumb.add_item(t("app.links.#{action}"))
   end
 end
