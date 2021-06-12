f575edcc105484cdfe23f1fa0f6902f8d7631e60
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-06-12 17:05:30 -0300

    Add action to delete contact

--- app/components/territories/contacts/form_page_component.rb
+++ app/components/territories/contacts/form_page_component.rb
@@ -14,7 +14,7 @@ class Territories::Contacts::FormPageComponent < ApplicationComponent
 
   def target_url
     if contact.id
-      return edit_territories_commercial_territory_contact_path(@territory, contact)
+      return territories_commercial_territory_contact_path(@territory, contact)
     end
 
     territories_commercial_territory_contacts_path(@territory)
