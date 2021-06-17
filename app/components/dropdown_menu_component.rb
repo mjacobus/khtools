30a4d6d5e009f879e9c0f573eecd93c46a6159fb
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-06-16 21:28:33 -0300

    Improve actions button for territory lists

--- app/components/dropdown_menu_component.rb
+++ app/components/dropdown_menu_component.rb
@@ -3,13 +3,22 @@
 class DropdownMenuComponent < ApplicationComponent
   renders_many :items
   attr_reader :title
+  attr_reader :icon
 
-  def initialize(title: '', classes: [])
+  def initialize(title: '', classes: [], icon: 'three-dots', pull: nil)
     @title = title
     @classes = Array.wrap(classes)
+    @icon = icon
+    @pull = pull
   end
 
   def classes
     @classes.join(' ')
+  end
+
+  def container_classes
+    if @pull
+      "pull-#{@pull}"
+    end
   end
 end
