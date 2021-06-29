9fc991866bce4789ab3a3b977880518f37b390af
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-06-29 14:26:16 -0300

    Improve index page

--- app/components/page_component.rb
+++ app/components/page_component.rb
@@ -6,4 +6,14 @@ class PageComponent < ApplicationComponent
       b.add_item('Home', urls.root_path)
     end
   end
+
+  def icon(name, &block)
+    icon = tag.i('', class: "bi bi-#{name}")
+
+    unless block
+      return icon
+    end
+
+    icon + '&nbsp;'.html_safe + yield
+  end
 end
