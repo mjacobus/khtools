962b2422854b4621cb82c8ca7f4a2bd3c18ad48b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-06-30 11:32:09 -0300

    Test rendering of component

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable RSpec/AnyInstance
 module RequestSpecHelper
   # rubocop:disable Metrics/MethodLength
   # rubocop:disable Metrics/AbcSize
@@ -25,9 +26,7 @@ module RequestSpecHelper
   # rubocop:enable Metrics/AbcSize
 
   def login_user(user)
-    # rubocop:disable RSpec/AnyInstance
     allow_any_instance_of(described_class).to receive(:current_user).and_return(user)
-    # rubocop:enable RSpec/AnyInstance
   end
 
   def grant_access(user, actions: ['*'])
@@ -36,4 +35,33 @@ module RequestSpecHelper
       user.grant_controller_access(controller, action: action)
     end
   end
+
+  def mock_renderer
+    allow_any_instance_of(ApplicationController).to receive(:render) do |instance, template, args|
+      instance.send(:response_body=, 'rendered by mock renderer')
+      renderer.render(template, args)
+    end
+  end
+
+  def renderer
+    @renderer ||= MockRenderer.new(self)
+  end
+
+  class MockRenderer
+    def initialize(spec)
+      @spec = spec
+      @rendered_templates = []
+      @rendered_options = []
+    end
+
+    def render(template, options)
+      @rendered_templates.push(template)
+      @rendered_options.push(options)
+    end
+
+    def has_rendered_component?(component)
+      @spec.expect(@rendered_templates.last).to @spec.be_equal_to(component)
+    end
+  end
 end
+# rubocop:enable RSpec/AnyInstance
