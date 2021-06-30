4424d78e7fe7e883b4c2d4af5da2b4204f634c81
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-06-30 20:38:16 -0300

    Implement talk form

--- app/components/public_talks/talks/form_page_component.rb
+++ app/components/public_talks/talks/form_page_component.rb
@@ -1,5 +1,37 @@
 # frozen_string_literal: true
 
-class PublicTalks::Talks::FormPageComponent < ApplicationComponent
+class PublicTalks::Talks::FormPageComponent < PageComponent
+  attr_reader :talk
 
+  def initialize(talk)
+    @talk = talk
+    breadcrumb.add_item(t('app.links.public_talks'), urls.public_talks_talks_path)
+
+    if talk.id
+      breadcrumb.add_item(
+        talk.date,
+        urls.public_talks_talk_path(talk)
+      )
+      breadcrumb.add_item(t('app.links.edit'))
+      return
+    end
+
+    breadcrumb.add_item(t('app.links.new'))
+  end
+
+  def target_url
+    if talk.id
+      return public_talks_talk_path(talk)
+    end
+
+    public_talks_talks_path
+  end
+
+  def collection_for_congregation
+    Db::Congregation.order(:name).pluck(:name, :id)
+  end
+
+  def collection_for_speaker
+    Db::PublicSpeaker.order(:name).pluck(:name, :id)
+  end
 end
--- app/components/public_talks/talks/show_page_component.rb
+++ app/components/public_talks/talks/show_page_component.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class PublicTalks::Talks::ShowPageComponent < ApplicationComponent
-
 end
