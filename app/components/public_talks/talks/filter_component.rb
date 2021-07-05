8f94fe3b047fcdb60868ebbf5f36520c15a54986
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-07-05 15:34:53 -0300

    Implement public talk filter by congregation

--- app/components/public_talks/talks/filter_component.rb
+++ app/components/public_talks/talks/filter_component.rb
@@ -11,11 +11,27 @@ class PublicTalks::Talks::FilterComponent < ApplicationComponent
     public_talks_talks_path
   end
 
+  def label_for_speaker
+    Db::PublicSpeaker.model_name.human
+  end
+
+  def label_for_congregation
+    Db::Congregation.model_name.human
+  end
+
   def speaker_selected?(speaker)
     speaker.id.to_s == params[:speaker_id]
   end
 
+  def congregation_selected?(congregation)
+    congregation.id.to_s == params[:congregation_id]
+  end
+
   def options_for_speaker
     Db::PublicSpeaker.all.order(:name)
+  end
+
+  def options_for_congregation
+    Db::Congregation.all.order(:name)
   end
 end
