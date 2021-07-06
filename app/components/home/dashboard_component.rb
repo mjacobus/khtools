073cacb9d4d8626f4b5ddf8dd71f1de6882e67d6
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-07-06 10:23:18 -0300

    Hide week talks when there are none

--- app/components/home/dashboard_component.rb
+++ app/components/home/dashboard_component.rb
@@ -2,14 +2,22 @@
 
 class Home::DashboardComponent < PageComponent
   def talks(&block)
-    segregate_talks(Db::PublicTalk.within_week, &block)
+    segregate_talks(week_talks, &block)
   end
 
   def talks_title
     t('app.titles.week_talks')
   end
 
+  def display_week_talks?
+    week_talks.any?
+  end
+
   private
+
+  def week_talks
+    @week_talks ||= Db::PublicTalk.within_week
+  end
 
   def segregate_talks(talks)
     yield(talks.select(&:local?), talks.reject(&:local?))
