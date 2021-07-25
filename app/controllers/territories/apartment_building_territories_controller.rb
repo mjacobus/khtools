cf30f7a5483067a40ae490f1697a765fb58ef382
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-07-25 01:23:06 -0300

    Add crud for apartment building territory

--- app/controllers/territories/apartment_building_territories_controller.rb
+++ app/controllers/territories/apartment_building_territories_controller.rb
@@ -1,9 +1,38 @@
 # frozen_string_literal: true
 
-class Territories::ApartmentBuildingTerritoriesController < ApplicationController
-  def index
-    territories = paginate(Db::ApartmentBuildingTerritory.all.with_dependencies)
-    page = Territories::ApartmentBuildingTerritories::IndexPageComponent.new(territories)
-    render page
+class Territories::ApartmentBuildingTerritoriesController < Territories::TerritoriesController
+  private
+
+  def index_page_class
+    Territories::ApartmentBuildingTerritories::IndexPageComponent
+  end
+
+  def model_class
+    Db::ApartmentBuildingTerritory
+  end
+
+  def attributes
+    %i[
+      name
+      assigned_at
+      returned_at
+      created_at
+      updated_at
+      assignee_id
+      address
+      building_name
+      number_of_apartments
+      has_a_roof
+      intercom_type
+      apartments
+      notes
+      territory_id
+      area_id
+      intercom_type_id
+      letter_box_type_id
+      primary_preaching_method_id
+      secondary_preaching_method_id
+      tertiary_preaching_method_id
+    ]
   end
 end
