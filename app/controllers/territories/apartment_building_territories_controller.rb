7644a736fde640498c535b3df361fd2717844a3f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-07-25 22:12:21 -0300

    Refactor editable attributes

--- app/controllers/territories/apartment_building_territories_controller.rb
+++ app/controllers/territories/apartment_building_territories_controller.rb
@@ -10,29 +10,4 @@ class Territories::ApartmentBuildingTerritoriesController < Territories::Territo
   def model_class
     Db::ApartmentBuildingTerritory
   end
-
-  def attributes
-    %i[
-      name
-      assigned_at
-      returned_at
-      created_at
-      updated_at
-      assignee_id
-      address
-      building_name
-      number_of_apartments
-      has_a_roof
-      intercom_type
-      apartments
-      notes
-      territory_id
-      area_id
-      intercom_type_id
-      letter_box_type_id
-      primary_preaching_method_id
-      secondary_preaching_method_id
-      tertiary_preaching_method_id
-    ]
-  end
 end
--- app/models/db/apartment_building_territory.rb
+++ app/models/db/apartment_building_territory.rb
@@ -2,4 +2,31 @@
 
 class Db::ApartmentBuildingTerritory < Db::Territory
   validates :address, presence: true
+
+  # rubocop:disable Metrics/MethodLength
+  def editable_attributes
+    %i[
+      name
+      assigned_at
+      returned_at
+      created_at
+      updated_at
+      assignee_id
+      address
+      building_name
+      number_of_apartments
+      has_a_roof
+      intercom_type
+      apartments
+      notes
+      territory_id
+      area_id
+      intercom_type_id
+      letter_box_type_id
+      primary_preaching_method_id
+      secondary_preaching_method_id
+      tertiary_preaching_method_id
+    ]
+  end
+  # rubocop:enable Metrics/MethodLength
 end
