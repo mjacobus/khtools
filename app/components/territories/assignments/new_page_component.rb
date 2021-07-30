0d4d10adc3b51561177e99a4eb31e3c1d5b63c17
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-07-30 19:12:48 -0300

    Add breadcrumb to assignment page

--- app/components/territories/assignments/new_page_component.rb
+++ app/components/territories/assignments/new_page_component.rb
@@ -3,7 +3,23 @@
 class Territories::Assignments::NewPageComponent < PageComponent
   include TerritoryAttributesConcern
 
-  has :territory
+  attr_reader :territory
+
+  def initialize(territory:)
+    @territory = territory
+    breadcrumb.add_item(
+      t("app.links.#{type}_territories"),
+      urls.territories_path(type)
+    )
+
+    if territory.id
+      breadcrumb.add_item(territory.name, urls.territory_path(territory))
+      breadcrumb.add_item(t('app.links.assign_territory'))
+      return
+    end
+
+    breadcrumb.add_item(t('app.links.new'))
+  end
 
   def publishers
     Db::Publisher.all.pluck(:name, :id)
