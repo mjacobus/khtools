4d38eeda7c4dad2ce234ae8d71bd943442c85ab4
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-07-30 10:46:19 -0300

    Add all elements to the territory show page

--- app/components/attribute_component.rb
+++ app/components/attribute_component.rb
@@ -3,13 +3,41 @@
 class AttributeComponent < ApplicationComponent
   attr_reader :icon_name
   attr_reader :classes
+  attr_reader :label
+  attr_reader :link
+  attr_reader :show_label
 
-  def initialize(icon_name: nil, classes: nil)
+  def initialize(icon_name: nil, classes: nil, label: nil, link: nil, show_label: false)
     @icon_name = icon_name
     @classes = Array.wrap(classes)
+    @label = label
+    @link = link
+    @show_label = show_label
   end
 
   def render?
     content.present?
+  end
+
+  def label_content
+    if icon_name
+      icon(icon_name) { label }
+    else
+      label
+    end
+  end
+
+  def text_content
+    if show_icon_in_text?
+      return icon(icon_name, class: 'icon-soft') { content }
+    end
+
+    content
+  end
+
+  private
+
+  def show_icon_in_text?
+    icon_name && !label
   end
 end
--- app/components/record_attribute_component.rb
+++ app/components/record_attribute_component.rb
@@ -1,8 +1,54 @@
 # frozen_string_literal: true
 
 class RecordAttributeComponent < ApplicationComponent
-  def initialize(record)
+  def initialize(record, attribute_name:)
     @record = record
+    @include_link = nil
+    @include_icon = true
+    @attribute_name = attribute_name
+  end
+
+  def without_icon
+    @include_icon = false
+    self
+  end
+
+  def include_icon?
+    @include_icon
+  end
+
+  def with_link(link = nil)
+    if link
+      @link = link
+    end
+
+    @include_link = true
+    self
+  end
+
+  def with_label
+    if label
+      @label = label
+    end
+
+    @include_label = true
+    self
+  end
+
+  def label
+    attribute_name(record, @attribute_name)
+  end
+
+  def include_label?
+    @include_label
+  end
+
+  def include_link?
+    @include_link
+  end
+
+  def link
+    # NOOP
   end
 
   def call
@@ -16,9 +62,19 @@ class RecordAttributeComponent < ApplicationComponent
   attr_reader :record
 
   def options
-    {
-      icon_name: icon_name,
-      classes: bem
-    }
+    {}.tap do |opts|
+      if include_icon?
+        opts[:icon_name] = icon_name
+      end
+      opts[:label] = label
+      opts[:classes] = bem
+      if include_link?
+        opts[:link] = link
+      end
+
+      if include_label?
+        opts[:show_label] = include_label?
+      end
+    end
   end
 end
