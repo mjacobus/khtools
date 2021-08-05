b9620c46ae872d1599a29dfbcf87bad755174cdd
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-04 21:22:06 -0300

    Extract controller logic to a crud concern

--- app/controllers/field_service/campaigns_controller.rb
+++ app/controllers/field_service/campaigns_controller.rb
@@ -1,70 +1,17 @@
 # frozen_string_literal: true
 
 class FieldService::CampaignsController < ApplicationController
-  def index
-    campaigns = paginate(model_class.order(created_at: :desc))
-    render FieldService::Campaigns::IndexPageComponent.new(campaigns: campaigns)
-  end
+  include CrudController
 
-  def show
-    render FieldService::Campaigns::ShowPageComponent.new(campaign: campaign)
-  end
+  key :campaign
 
-  def new
-    @campaign = model_class.new
-    render form(campaign)
-  end
+  permit :code,
+         :description,
+         :name,
+         :start_date,
+         :end_date
 
-  def create
-    @campaign = model_class.new
-    save_record
-  end
+  scope { Db::PreachingCampaign.order(created_at: :desc) }
 
-  def edit
-    render form(campaign)
-  end
-
-  def update
-    save_record
-  end
-
-  def destroy
-    campaign.destroy
-    redirect_to(action: :index)
-  end
-
-  private
-
-  def campaign
-    @campaign ||= model_class.find(params[:id])
-  end
-
-  def save_record
-    campaign.attributes = attributes
-
-    if campaign.save
-      redirect_to action: :index
-      return
-    end
-
-    render form(campaign), status: :unprocessable_entity
-  end
-
-  def form(campaign)
-    FieldService::Campaigns::FormPageComponent.new(campaign: campaign)
-  end
-
-  def attributes
-    params.require(:campaign).permit(
-      :code,
-      :description,
-      :name,
-      :start_date,
-      :end_date
-    )
-  end
-
-  def model_class
-    Db::PreachingCampaign
-  end
+  component_class_template 'FieldService::Campaigns::%{type}PageComponent'
 end
