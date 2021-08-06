f26de9ee70a97f204207903be4b5c33729e8d7f4
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-05 22:16:23 -0300

    Refactor congregation controller

--- app/controllers/public_talks/congregations_controller.rb
+++ app/controllers/public_talks/congregations_controller.rb
@@ -1,63 +1,20 @@
 # frozen_string_literal: true
 
 class PublicTalks::CongregationsController < ApplicationController
-  def index
-    congregations = paginate(Db::Congregation.all.order(:name))
-    render Congregations::IndexPageComponent.new(congregations)
-  end
+  include CrudController
 
-  def show
-    congregation = Db::Congregation.find(params[:id])
-    render Congregations::ShowPageComponent.new(congregation)
-  end
+  key :congregation
 
-  def new
-    congregation = Db::Congregation.new
-    render Congregations::FormPageComponent.new(congregation)
-  end
+  model_class Db::Congregation
+  scope { Db::Congregation.all.order(:name) }
 
-  def create
-    congregation = Db::Congregation.new(congregation_params)
+  permit :name,
+         :address,
+         :primary_contact_person,
+         :primary_contact_phone,
+         :primary_contact_email,
+         :weekend_meeting_time,
+         :local
 
-    if congregation.save
-      return redirect_to(action: :index)
-    end
-
-    render Congregations::FormPageComponent.new(congregation), status: :unprocessable_entity
-  end
-
-  def edit
-    congregation = Db::Congregation.find(params[:id])
-    render Congregations::FormPageComponent.new(congregation)
-  end
-
-  def update
-    congregation = Db::Congregation.find(params[:id])
-
-    if congregation.update(congregation_params)
-      return redirect_to(action: :index)
-    end
-
-    render Congregations::FormPageComponent.new(congregation), status: :unprocessable_entity
-  end
-
-  def destroy
-    congregation = Db::Congregation.find(params[:id])
-    congregation.destroy
-    redirect_to(action: :index)
-  end
-
-  private
-
-  def congregation_params
-    params.require(:congregation).permit(
-      :name,
-      :address,
-      :primary_contact_person,
-      :primary_contact_phone,
-      :primary_contact_email,
-      :weekend_meeting_time,
-      :local
-    )
-  end
+  component_class_template 'Congregations::%{type}PageComponent', use_key: false
 end
