6b32e6f93075c2cdc96a6c48a3a361ff9756ff73
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-05 22:49:19 -0300

    Refactor territories controller

--- app/controllers/territories/territories_controller.rb
+++ app/controllers/territories/territories_controller.rb
@@ -1,71 +1,21 @@
 # frozen_string_literal: true
 
 class Territories::TerritoriesController < ApplicationController
-  def index
-    territories = paginate(model_class.all.with_dependencies.search(params))
-    render index_page_component(territories)
-  end
+  include CrudController
 
-  def show
-    render Territories::ShowPageComponent.new(territory: territory)
-  end
+  key :territory
 
-  def new
-    render form(model_class.new)
-  end
+  scope { model_class.all.with_dependencies.search(params) }
 
-  def create
-    @territory = model_class.new
-    save_territory
-  end
-
-  def edit
-    render form(territory)
-  end
-
-  def update
-    save_territory
-  end
-
-  def destroy
-    territory.destroy
-    redirect_to action: :index
-  end
+  component_class_template 'Territories::%{type}PageComponent'
 
   private
 
-  def territory
-    @territory ||= model_class.find(params[:id])
-  end
-
-  def model_class
-    raise 'Abstract method'
-  end
-
-  def save_territory
-    territory.attributes = territory_attributes
-
-    if territory.save
-      redirect_to action: :index
-      return
-    end
-
-    render form(territory), status: :unprocessable_entity
-  end
-
-  def form(territory)
-    Territories::FormPageComponent.new(territory: territory)
-  end
-
-  def territory_attributes
-    params.require(:territory).permit(*attributes)
-  end
-
-  def attributes
+  def permitted_keys
     model_class.new.editable_attributes
   end
 
-  def index_page_component(territories)
+  def index_component(territories)
     Territories::IndexPageComponent.new(
       territories: territories,
       title: model_class.model_name.human,
