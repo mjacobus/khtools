3d84148c9d79ca53828ad96ef317a0a453aeec9d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-06 16:52:17 -0300

    Sort assignments by territory name

--- app/models/db/preaching_campaign_assignment.rb
+++ app/models/db/preaching_campaign_assignment.rb
@@ -5,5 +5,5 @@ class Db::PreachingCampaignAssignment < ApplicationRecord
   belongs_to :territory
   belongs_to :assignee, class_name: 'Publisher', optional: true
 
-  scope :with_dependencies, -> { includes(:territory, :assignee) }
+  scope :with_dependencies, -> { includes(:territory, :assignee).order('territories.name') }
 end
