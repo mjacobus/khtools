1070049bd05d939c2774581fcc4d225599361e70
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-08 14:01:50 -0300

    Add link to publisher and territory in territory show page

---
+++ spec/models/routes_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Routes do
+  subject(:routes) { described_class.new }
+
+  describe '#to' do
+    it 'resolves publisher' do
+      record = Db::Publisher.new(id: 1)
+
+      expected = '/admin/db_publishers/1'
+
+      expect(routes.to(record)).to eq(expected)
+    end
+
+    it 'resolves territory path' do
+      record = Db::CommercialTerritory.new(id: 1)
+
+      expected = '/territories/commercial_territories/1'
+
+      expect(routes.to(record)).to eq(expected)
+    end
+  end
+end
