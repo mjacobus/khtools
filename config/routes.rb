858dc9967551f1b1631238886c2cb25b192ceabb
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-21 13:36:35 -0300

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,68 +3,10 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root to: 'home#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
   if Rails.env.development?
     get '/dev/login', to: 'development#login'
   end
-
-  get '/discursos', to: 'public#public_talks'
-
-  namespace :field_service do
-    resources :campaigns do
-      resources :assignments, controller: :campaign_assignments, only: %i[index edit update] do
-        collection do
-          post 'create_assignments/:territory_type', action: :create_assignments, as: :create
-        end
-      end
-    end
-  end
-
-  namespace :congregation do
-    resources :publishers
-  end
-
-  namespace :public_talks do
-    resources :congregations
-    resources :speakers
-    resources :talks
-  end
-
-  namespace :meeting_attendance do
-    resources :meetings do
-      resources :simple_counter_attendees, controller: :simple_counter
-    end
-  end
-
-  # TODO: Move to field service namespace
-  namespace :territories do
-    resources :territories, only: [:show] do
-      resources :assignments
-    end
-    resources :regular_territories
-    resources :phone_list_territories do
-      member do
-        get :xls
-        get :pdf, defaults: { format: :pdf }
-        post :create_token
-      end
-    end
-    resources :apartment_building_territories
-    resources :commercial_territories do
-      resources :contacts
-    end
-    resources :phone_lists
-  end
-
-  resources :users, only: %i[index] do
-    member do
-      patch :enable
-      patch :disable
-      patch :grant_admin
-      patch :revoke_admin
-    end
-  end
-
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
 end
