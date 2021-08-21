bc0cc444b7618157074fa04ae850b3c56c92484b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-21 16:03:04 -0300

    Delete more stuff

--- app/models/db/meeting_attendance.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Db::MeetingAttendance
-  def self.table_name_prefix
-    'ma_'
-  end
-end
--- app/models/db/meeting_attendance/simple_counter_attendee.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Db::MeetingAttendance::SimpleCounterAttendee < ApplicationRecord
-  belongs_to :meeting
-end
