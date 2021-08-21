f4990692521e03bf1d57ced24d9af298c2d9c41b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-08-21 16:24:52 -0300

    Remove, modify

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
-  include Db::Identifiable
-
   self.abstract_class = true
 end
--- db/migrate/20210301235018_rename_simple_counter_attendee_amount_to_quantity.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RenameSimpleCounterAttendeeAmountToQuantity < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :ma_simple_counter_attendees, :amount, :quantity
-  end
-end
