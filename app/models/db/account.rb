1c19ffa83043191530be84590449f6cbdb27d82b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-10-13 17:59:51 -0300

    Display congregation name in relevant views

--- app/models/db/account.rb
+++ app/models/db/account.rb
@@ -4,4 +4,8 @@ class Db::Account < ApplicationRecord
   validates :congregation_name, uniqueness: { case_sensitive: false }, presence: true
 
   has_many :users, dependent: :restrict_with_exception
+
+  def to_s
+    congregation_name
+  end
 end
