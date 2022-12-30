bf17164025f37c7d2b95924235f37540be773cc5
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-12-30 09:54:26 -0300

    Appease rubocop

--- app/admin/db_accounts.rb
+++ app/admin/db_accounts.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Db::Account do
-  permit_params :congregation_name, 
-    :territory_map,
-    :cloudinary_cloud_name,
-    :cloudinary_api_key,
-    :cloudinary_api_secret
-
+  permit_params :congregation_name,
+                :territory_map,
+                :cloudinary_cloud_name,
+                :cloudinary_api_key,
+                :cloudinary_api_secret
 
   form do |f|
     f.semantic_errors
