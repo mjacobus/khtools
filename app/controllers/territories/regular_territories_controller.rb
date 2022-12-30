911a96ae590b0cf7c186d9d0b5762a9d7ef72640
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-12-30 17:39:24 -0300

    Improve layout of printable territory

--- app/controllers/territories/regular_territories_controller.rb
+++ app/controllers/territories/regular_territories_controller.rb
@@ -6,8 +6,6 @@ class Territories::RegularTerritoriesController < Territories::TerritoriesContro
   def printable
     territory = current_user.account.territories.regular.find(params[:regular_territory_id])
     @component = Territories::PrintableTerritoryPageComponent.new(territory: territory)
-    options = {}
-    # options[:disposition] = 'attachment'
-    export_pdf(territory.filename('pdf'), options)
+    export_pdf(territory.filename('pdf'), header: { right: territory.name })
   end
 end
