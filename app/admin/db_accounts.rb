e73f78fd7142e0461809d45d9b7b2385f3b9529a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-12-30 09:18:45 -0300

    Manage cloudinary credentials in the account admin page

--- app/admin/db_accounts.rb
+++ app/admin/db_accounts.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Db::Account do
-  permit_params :congregation_name, :territory_map
+  permit_params :congregation_name, 
+    :territory_map,
+    :cloudinary_cloud_name,
+    :cloudinary_api_key,
+    :cloudinary_api_secret
+
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :congregation_name
+      f.input :territory_map
+      f.input :cloudinary_cloud_name
+      f.input :cloudinary_api_key
+      f.input :cloudinary_api_secret
+    end
+    f.actions
+  end
 end
