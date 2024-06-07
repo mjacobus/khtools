d90b25333e8965f5af58b03cbf16008a4dc08c09
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-07 08:56:21 -0300

    Enable @rails/ujs
    That fixes confirmations and custom http method links.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+# if Rails.env.production?
+#   pin "jquery", to: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+#   pin "jquery_ujs", to: "https://cdnjs.cloudflare.com/ajax/libs/jquery-ujs/1.2.3/rails.min.js"
+# else
+#   pin "jquery", to: "jquery.js" # after ./bin/importmap pin jquery
+#   pin "jquery_ujs", to: "jquery_ujs.js"
+# end
+
+pin 'application'
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
