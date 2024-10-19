57b9f414de52b20b0404c27d496a7acf22054ad2
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-10-19 17:51:14 -0300

    Rubocop files

--- spec/models/db/location_spec.rb
+++ spec/models/db/location_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Db::Location, type: :model do
   let(:location) { FactoryBot.create(:location) }
 
-  it "has a valid factory" do
+  it 'has a valid factory' do
     expect(location).to be_valid
   end
 
-  it "belongs to territory" do
+  it 'belongs to territory' do
     expect(location).to belong_to(:territory)
   end
 end
